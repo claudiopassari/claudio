f67c975d957b0a04bb2e6b667b4f13001c551557
claudiopassari <claudio.passari@gmail.com>
2026-09-23 02:20:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@
    la versione precedente dalla cache.
    ========================================================= */
 
-const NOME_CACHE = "claudio-v6.0";
+const NOME_CACHE = "claudio-v6.1";
 
 const GUSCIO_APPLICAZIONE = [
   "./",
